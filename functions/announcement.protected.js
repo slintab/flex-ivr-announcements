3a198ebd99c98b8595abf929bbc807ae9462e750
slintab <slintab@icloud.com>
2023-08-13 15:13:37 +0100

    refactored functions

--- functions/announcement.protected.js
+++ functions/announcement.protected.js
@@ -2,13 +2,16 @@ exports.handler = async (context, event, callback) => {
   const syncDocumentName = context.DOCUMENT_NAME;
   const syncServiceSid = context.SYNC_SERVICE_SID;
 
-  const syncClient = Runtime.getSync({ serviceName: syncServiceSid });
+  if (!(syncDocumentName && syncServiceSid)) {
+    return callback("Missing parameters");
+  }
 
+  const syncClient = Runtime.getSync({ serviceName: syncServiceSid });
   const document = await syncClient.documents(syncDocumentName).fetch();
   const announcement = document.data.announcement;
 
   if (announcement === "") {
-    return callback("No announcement.");
+    return callback("No announcement");
   }
 
   return callback(null, document.data);
--- functions/token.js
+++ functions/token.js
@@ -1,38 +1,28 @@
 const TokenValidator = require("twilio-flex-token-validator").functionValidator;
 
 exports.handler = TokenValidator(function (context, event, callback) {
-  const ACCOUNT_SID = context.ACCOUNT_SID;
-  const SERVICE_SID = context.SYNC_SERVICE_SID;
-  const API_KEY = context.API_KEY;
-  const API_SECRET = context.API_SECRET;
-
+  const { ACCOUNT_SID, SYNC_SERVICE_SID, API_KEY, API_SECRET } = context;
   const IDENTITY = "flex-ivr-announcements";
-
   const AccessToken = Twilio.jwt.AccessToken;
   const SyncGrant = AccessToken.SyncGrant;
 
   const syncGrant = new SyncGrant({
-    serviceSid: SERVICE_SID,
+    serviceSid: SYNC_SERVICE_SID,
   });
 
   const accessToken = new AccessToken(ACCOUNT_SID, API_KEY, API_SECRET);
-
   accessToken.addGrant(syncGrant);
   accessToken.identity = IDENTITY;
 
   const response = new Twilio.Response();
-
-  // Set cross origin headers
   const headers = {
     "Content-Type": "application/json",
     "Access-Control-Allow-Origin": "*",
     "Access-Control-Allow-Methods": "GET",
     "Access-Control-Allow-Headers": "Content-Type",
   };
-
-  // Set headers in response
-  response.setBody({ token: accessToken.toJwt() });
   response.setHeaders(headers);
+  response.setBody({ token: accessToken.toJwt() });
 
   callback(null, response);
 });
